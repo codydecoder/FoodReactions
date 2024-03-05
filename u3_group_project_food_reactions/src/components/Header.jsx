73b3eae0308b1fb5641980f7df22f83b58753f96
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-03-05 16:26:42 -0600

    Header component ready for styling and links

--- u3_group_project_food_reactions/src/components/Header.jsx
+++ u3_group_project_food_reactions/src/components/Header.jsx
@@ -2,9 +2,14 @@ export default function Header(){
     
     return(
         <div className='header'>
-            <h1>Food Reactions</h1>
+            <h1 className='header-logo'>Food Reactions</h1>
+            <nav className='search-nav'>
+                <h2 className='nav-clicks'>Recipes</h2>
+                <h2 className='nav-clicks'>Ingredients</h2>
+                <h2 className='nav-clicks'>Countries</h2>
+            </nav>
             <div className='search-area'>
-                <input id='search-input' type='Text' placeholder='Enter Recipe...'/>
+                <input id='search-input' type='Text' placeholder='Enter recipe name...'/>
                 <button id='search-submit'>Search</button>
             </div>
         </div>
