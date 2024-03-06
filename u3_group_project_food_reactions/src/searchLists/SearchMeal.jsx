d20590f9c3ea62894454cb0e4bfb21660a51ae89
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-03-06 17:33:09 -0600

    Styled SearchMeal

--- u3_group_project_food_reactions/src/searchLists/SearchMeal.jsx
+++ u3_group_project_food_reactions/src/searchLists/SearchMeal.jsx
@@ -29,19 +29,19 @@ export default function SearchMeal(){
 
     } else {
         return (
-        <div className="recipe-list-container">
-        <ul className='recipes-list'>
+        <div className="recipe-list">
+        
             {
                 recipes.map((recipe, key) => (
                     <li className="recipe-card" key={recipe.idMeal}>
-                        <h3 className="card-labels">Name: <span className='card-details'>{recipe.strMeal}</span></h3>
-                        <h3 className="card-labels">Area: <span className='card-details'>{recipe.strArea}</span></h3>
-                        <h3 className="card-labels">Ingredients: <span className='card-details'>{recipe.strIngredient1} {recipe.strIngredient2} {recipe.strIngredient3} {recipe.strIngredient4} {recipe.strIngredient5} {recipe.strIngredient6} {recipe.strIngredient7} {recipe.strIngredient8} {recipe.strIngredient9} {recipe.strIngredient10} {recipe.strIngredient11} {recipe.strIngredient12} {recipe.strIngredient13} {recipe.strIngredient14} {recipe.strIngredient15} {recipe.strIngredient16} {recipe.strIngredient17} {recipe.strIngredient18} {recipe.strIngredient19} {recipe.strIngredient20}</span></h3>
-                        <h3 className="card-labels">Instructions: <span className='card-details'>{recipe.strInstructions}</span></h3>
+                        <h3 className="recipe-name">Name: <span className='recipe-name-details'>{recipe.strMeal}</span></h3>
+                        <h3 className="recipe-area">Area: <span className='recipe-area-details'>{recipe.strArea}</span></h3>
+                        <h3 className="recipe-ingredients">Ingredients: <span className='recipe-ingredients-details'>{recipe.strIngredient1} {recipe.strIngredient2} {recipe.strIngredient3} {recipe.strIngredient4} {recipe.strIngredient5} {recipe.strIngredient6} {recipe.strIngredient7} {recipe.strIngredient8} {recipe.strIngredient9} {recipe.strIngredient10} {recipe.strIngredient11} {recipe.strIngredient12} {recipe.strIngredient13} {recipe.strIngredient14} {recipe.strIngredient15} {recipe.strIngredient16} {recipe.strIngredient17} {recipe.strIngredient18} {recipe.strIngredient19} {recipe.strIngredient20}</span></h3>
+                        <h3 className="recipe-instructions">Instructions: <br/><span className='recipe-instructions-details'>{recipe.strInstructions}</span></h3>
                     </li>
                 ))
             }
-        </ul>
+        
         </div>
     )}
 
