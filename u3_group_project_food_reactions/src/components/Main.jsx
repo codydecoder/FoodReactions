1922b96d6b2ef71b1383866e4eaa4fc0177bfacf
andrew-taggart <andrew.taggart53@gmail.com>
2024-03-06 16:48:14 -0500

    save work

--- u3_group_project_food_reactions/src/components/Main.jsx
+++ u3_group_project_food_reactions/src/components/Main.jsx
@@ -1,15 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
-//components
-import Home from './Home'
-
+import SearchMeal from '../searchLists/SearchMeal'
 
 const Main = () => {
   return (
     <div>
       <Routes>
           <Route path="/" element={ <Home /> } />
-          {/* <Route path="/listings" element={ <Listings states={states} />} />
-          <Route path="/listings/:id" element={ <StateDetails states={states} />} /> */}
+          <Route path="/listings" element={ <SearchMeal states={states} />} />
+          <Route path="/listings/:id" element={ <StateDetails states={states} />} />
+          <Route path="/countries" element={ <Listings states={states} />} />
         </Routes>
     </div>
   )
